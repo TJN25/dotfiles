d60980935bd0316b020a1d1cf94b8fe9f7fe3cdf
Nicholson <nicholson.tom.n@gmail.com>
2024-11-18 13:12:09 +1300

    large update continued

--- tmux/settings.tmux
+++ tmux/settings.tmux
@@ -5,6 +5,7 @@ set -g bell-action none
 set -g visual-bell off
 set -g monitor-bell off
 set-option -s set-clipboard on
+set-option -g renumber-windows on
 # Start windows and panes at 1, not 0
 set -g base-index 1
 setw -g pane-base-index 1
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,19 +7,18 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Catppuccin Latte'
+config.color_scheme = "Catppuccin Mocha"
 -- require('gradient')
 
 -- require('rose-pine')
--- config.color_scheme = 'Catppuccin Latte'
+-- config.color_scheme = 'Catppuccin Mocha'
 
 -- My additions
-config.exit_behavior="Hold"
-config.font = wezterm.font('MesloLGS NF')
-config.font_size = 18
+config.exit_behavior = "Hold"
+config.font = wezterm.font("MesloLGS NF")
+config.font_size = 24
 config.window_background_opacity = 0.95
 config.enable_tab_bar = false
-
 
 -- config.adjust_window_size_when_changing_font_size = false
 -- config.debug_key_events = false
@@ -28,12 +27,12 @@ config.window_decorations = "RESIZE"
 
 -- and finally, return the configuration to wezterm
 
-config.leader = { key = 'a', mods = 'ALT|CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = "a", mods = "ALT|CTRL", timeout_milliseconds = 1000 }
 
 config.keys = {
     {
-        key = '^',
-        mods = 'CTRL',
+        key = "^",
+        mods = "CTRL",
         action = wezterm.action.DisableDefaultAssignment,
     },
 }
